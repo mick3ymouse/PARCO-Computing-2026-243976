c78f93198b890732e48274ff98593f3b31fc4f7d
mick3ymouse <michimar2003@outlook.com>
2026-01-20 15:52:35 +0100

    add: added new files for running weak scaling benchmark

---
+++ Deliverable2_MPI/src/main_weak.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << "This is the main_weak.cpp file. It is currently not used in the build." << endl;
+    return 0;
+}
