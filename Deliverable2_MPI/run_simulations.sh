dd8b25186ca8e18c43a3818fc008f608b39ac132
mick3ymouse <michimar2003@outlook.com>
2026-01-20 11:36:24 +0100

    feat: automated simulations

--- Deliverable2_MPI/run_simulations.sh
+++ Deliverable2_MPI/run_simulations.sh
@@ -1,2 +1,59 @@
 #!/bin/bash
 
+# ======================================
+# LOAD MODULES
+# ======================================
+
+echo ">>> Loading required modules..."
+
+MODULES_TO_LOAD=("openmpi-4.0.4" "cmake-3.15.4" "make-4.3")
+
+# Check and load each module
+for module_name in "${MODULES_TO_LOAD[@]}"; do
+    if ! module list 2>&1 | grep -q "$module_name"; then
+        echo "    Loading $module_name..."
+        module load "$module_name"
+        
+        if [ $? -ne 0 ]; then
+            echo "    ERROR: Failed to load $module_name."
+            exit 1
+        fi
+    else
+        echo "    $module_name is already loaded."
+    fi
+done
+
+echo ">>> Modules loaded successfully."
+
+# Start compilation
+echo ""
+echo ">>> Starting Compilation..."
+
+# Create build directory if it doesn't exist
+mkdir -p build
+cd build
+
+# Run CMake and Make
+# We build in Release mode for max performance (-O3)
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j4
+
+# Check if compilation was successful
+if [ ! -f ./spmv_strong_exec ]; then
+    echo "   Compilation failed! Executable not found."
+    exit 1
+fi
+
+echo ""
+echo ">>> Compilation Successful."
+echo ""
+
+cd .. # Return to project root
+
+# ======================================
+# MAIN SCRIPT
+# ======================================
+
+# Submit the strong scaling job
+echo ">>> Submitting strong scaling job..."
+qsub scripts/run_strong_scaling.pbs
